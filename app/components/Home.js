b4668ddddc0ed06a36683fbeabc6184fef433514
Jim Haff <Jimmyhaff3@gmail.com>
2017-03-08 15:12:13 -0600

    added react stuff

---
+++ app/components/Home.js
@@ -0,0 +1,11 @@
+var React = require('react');
+
+var Home = React.createClass({
+  render: function(){
+    return (
+      <div> Hell from Home! Its ET!</div>
+      )
+  }
+})
+
+module.exports = Home
---
+++ app/containers/PromptContainer.js
@@ -0,0 +1,13 @@
+var React = require('react')
+
+var PromptContainer = React.createClass({
+  render: function(){
+    console.log(this)
+    return (
+      <h1>Hi {this.props.route.header</h1>
+      )
+  }
+});
+
+
+module.exports = PromptContainer;
